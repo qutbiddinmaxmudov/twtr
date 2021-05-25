d6934f7f5753ed44f963a1cb61f5c6946fabaeab
Max <qutbiddinmaxmudov@mail.ru>
2021-05-25 18:56:30 +0500

    part-1 35-26

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/styles";
+
+import App from "./App";
+import { theme } from "./theme";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
