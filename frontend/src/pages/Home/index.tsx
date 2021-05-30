c4172daf6b78d904fc9d18004542c0dcb972af66
Max <qutbiddinmaxmudov@mail.ru>
2021-05-30 21:31:28 +0500

    must finish sidebar icons part-2 40-57

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,53 +1,45 @@
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import Twitter from "@material-ui/icons/Twitter";
-import SearchIcon from '@material-ui/icons/Search';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 
 export const Home: React.FC = (): React.ReactElement => {
   return (
     <section>
       <Grid container spacing={3}>
         <Grid item xs={1}>
-          <ul>
-            <li>
-              <IconButton color="primary">
-                <Twitter />
-              </IconButton>
-            </li>
-            <li>
-              <IconButton>
-                <SearchIcon />
-              </IconButton>
-            </li>
-            <li>
-              <IconButton>
-                <NotificationsIcon />
-              </IconButton>
-            </li>
-            <li>
-              <IconButton color="primary">
-                <Twitter />
-              </IconButton>
-            </li>
-            <li>
-              <IconButton color="primary">
-                <Twitter />
-              </IconButton>
-            </li>
-            <li>
-              <IconButton color="primary">
-                <Twitter />
-              </IconButton>
-            </li>
-            <li>
-              <IconButton color="primary">
-                <Twitter />
-              </IconButton>
-            </li>
-          </ul>
+          <List style={{ width: "max-content" }}>
+            <ListItem component={IconButton} color="primary">
+              <Twitter />
+            </ListItem>
+            <ListItem component={IconButton}>
+              <SearchIcon />
+            </ListItem>
+            <ListItem component={IconButton}>
+              <NotificationsIcon />
+            </ListItem>
+            <ListItem component={IconButton}>
+              <MailOutlineIcon />
+            </ListItem>
+            <ListItem component={IconButton}>
+              <BookmarkBorderIcon />
+            </ListItem>
+            <ListItem component={IconButton}>
+              <ListAltIcon />
+            </ListItem>
+            <ListItem component={IconButton}>
+              <PermIdentityIcon />
+            </ListItem>
+          </List>
         </Grid>
         <Grid item xs={8}>
           <Paper>xs</Paper>
